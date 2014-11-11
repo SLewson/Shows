09c61705c1f93aa59e224e06d7db09b27ab988bc
SLewson <slewson@calpoly.edu>
2014-11-11 11:48:52 -0800

    adding iOS app

---
+++ ShowApp/Shows/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Shows
+//
+//  Created by Ian Mitchell on 11/3/14.
+//  Copyright (c) 2014 Ian Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ShowApp/Shows/ShowViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ShowViewController.swift
+//  Shows
+//
+//  Created by Spencer Lewson on 11/6/14.
+//  Copyright (c) 2014 Ian Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ShowViewController: UIViewController {
+
+    public var test: NSString?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if let test = test {
+            print(test)
+        }
+        else {
+            print("ruh-roh")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
