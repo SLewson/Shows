82af6b59fbfd38434df5271fea7bee6064bb74a4
Ian Mitchell <ian.mitchel1@live.com>
2014-12-07 15:58:10 -0800

    Login Testing

---
+++ ShowApp/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Shows
+//
+//  Created by Ian Mitchell on 12/7/14.
+//  Copyright (c) 2014 Ian Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressLogin(sender: AnyObject) {
+        UserManagement.sendRequest(username: username.text, password: password.text)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
